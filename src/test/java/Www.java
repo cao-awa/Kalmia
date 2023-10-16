6e261f8f01d248e10e387df081d2e9b0613c5e65
cao_awa <cao.awa.www@gmail.com>
2023-10-16 17:13:20 +0800

    ???

--- src/test/java/Www.java
+++ src/test/java/Www.java
@@ -1,28 +1,5 @@
-import java.util.concurrent.StructuredTaskScope;
-
 public class Www {
     public static void main(String[] args) {
-        try (StructuredTaskScope.ShutdownOnSuccess<String> task = new StructuredTaskScope.ShutdownOnSuccess<>()) {
-            task.fork(() -> {
-                Thread.sleep(100);
-                return "#1";
-            });
-
-            task.fork(() -> {
-                Thread.sleep(200);
-                return "#2";
-            });
-
-            task.fork(() -> {
-                Thread.sleep(100);
-                return "#3";
-            });
-
-            task.join();
-
-            System.out.println(task.result());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println("???");
     }
 }
