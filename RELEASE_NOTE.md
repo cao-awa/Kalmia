70095482a6157b3203d8270c63248543c0c76c62
草awa <cao.awa.www@gmail.com>
2026-09-21 02:36:37 +0900

    Add logging in test engine.

---
+++ RELEASE_NOTE.md
@@ -0,0 +1,5 @@
+# Bug fixes
+Fix bug of lags by rcv buf and snd buf config wrong.
+
+# Other
+Add logging in test engine.
--- src/main/kotlin/com/github/cao/awa/kalmia/test/junit/KalmiaTestEngine.kt
+++ src/main/kotlin/com/github/cao/awa/kalmia/test/junit/KalmiaTestEngine.kt
@@ -2,6 +2,8 @@ package com.github.cao.awa.kalmia.test.junit
 
 import com.github.cao.awa.kalmia.entrypoint.KalmiaEntrypoint
 import com.github.cao.awa.kalmia.test.junit.descriptor.KalmiaTestDescriptor
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import org.junit.platform.engine.EngineDiscoveryRequest
 import org.junit.platform.engine.ExecutionRequest
 import org.junit.platform.engine.TestDescriptor
@@ -11,6 +13,10 @@ import org.junit.platform.engine.UniqueId
 import org.junit.platform.engine.support.descriptor.EngineDescriptor
 
 class KalmiaTestEngine: TestEngine {
+    companion object {
+        private val LOGGER: Logger = LogManager.getLogger("KalmiaTestEngine")
+    }
+
     override fun getId(): String {
         return "kalmia-engine"
     }
@@ -19,6 +25,7 @@ class KalmiaTestEngine: TestEngine {
         discoveryRequest: EngineDiscoveryRequest,
         uniqueId: UniqueId
     ): TestDescriptor {
+        LOGGER.info("Discovering KalmiaTestDescriptor")
         val root = EngineDescriptor(uniqueId, "Kalmia")
 
         root.addChild(KalmiaTestDescriptor(uniqueId.append("test", "entry")))
@@ -27,17 +34,26 @@ class KalmiaTestEngine: TestEngine {
     }
 
     override fun execute(request: ExecutionRequest) {
+        LOGGER.info("Executing kalmia test")
         val root = request.rootTestDescriptor
+
+        val test = root.children.single()
 
         val listener = request.engineExecutionListener
 
         listener.executionStarted(root)
 
+        listener.executionStarted(test)
+
         try {
             KalmiaEntrypoint.main()
 
+            listener.executionFinished(test, TestExecutionResult.successful())
+
             listener.executionFinished(root, TestExecutionResult.successful())
         } catch (e: Throwable) {
+            listener.executionFinished(test, TestExecutionResult.failed(e))
+
             listener.executionFinished(root, TestExecutionResult.failed(e))
         }
     }
